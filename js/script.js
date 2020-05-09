d502028f0f6c383b2579fe8d988b191da4a2a73b
Ariel <ariel.luz@ccc.ufcg.edu.br>
2020-05-09 20:30:10 -0300

    Connect with API

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
+
+let xhttp = new XMLHttpRequest();
+xhttp.open("GET", url, true);
+
+xhttp.onreadystatechange = function () {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+        let data = JSON.parse(xhttp.responseText);
+
+        for (let i = 0; i < data.length;i++){
+
+            console.log(data[i]);
+
+            let card = document.createElement("div");
+            card.innerHTML = " <div class=\"card\" style=\"width: 18rem;\">\n" +
+                "                <img class=\"card-img-top\" src="+data[i].photo+" alt=\"Card image cap\">\n" +
+                "                <div class=\"card-body\">\n" +
+                "                <p class=\"card-text\">"+data[i].name+"</p>\n" +
+                "            </div>\n" +
+                "            </div>"
+            document.getElementById("places").appendChild(card);
+        }
+    }
+}
+
+//Continues if the server no yet answer
+xhttp.send();
